756dd414c0609fd7632dc841573331e5453f57d1
Luyang Chen <LouisChen1992@hotmail.com>
2018-01-22 00:08:01 -0800

    debug

--- src/data_layer.py
+++ src/data_layer.py
@@ -114,8 +114,8 @@ class DataInRamInputLayer():
 			tDimSplit = np.load(os.path.join(self._path, self._bucket_tDimSplit[bucket][idx_file]))
 
 			if self._selected_int and self._selected_float:
-				selected_int_FF = [i for i in range(self._covariate_count_int) if i not in selected_int]
-				selected_float_FF = [i for i in range(self._covariate_count_float) if i not in selected_float]
+				selected_int_FF = [i for i in range(self._covariate_count_int) if i not in self._selected_int]
+				selected_float_FF = [i for i in range(self._covariate_count_float) if i not in self._selected_float]
 				X_int_RNN = X_int[:,:,self._selected_int]
 				X_float_RNN = X_float[:,:,self._selected_float]
 				X_int_FF = X_int[:,:,selected_int_FF]
